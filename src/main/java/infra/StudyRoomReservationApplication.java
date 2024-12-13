25c36633f484855a90306012bd50e2a360434064
leGit-y <99312658+leGit-y@users.noreply.github.com>
2024-12-13 16:51:41 +0900

    [Fix] jpaconfig, securityconfig

--- src/main/java/infra/StudyRoomReservationApplication.java
+++ src/main/java/infra/StudyRoomReservationApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing  // JPA 감사 기능 활성화
+
 @SpringBootApplication
 public class StudyRoomReservationApplication {
 
